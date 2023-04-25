0a324b58fcabb0216c5c975b37932ded5e3eb539
lewkrajeszczak <u38_lewkra_waw@technischools.com>
2023-04-25 15:50:17 +0200

    App.py

--- io_lottery/app.py
+++ io_lottery/app.py
@@ -1,70 +1,44 @@
-import users as users
-from flask import Flask, Response, request, jsonify
-from flask_restful import Resource, Api, abort
-
-from io_lottery.controllers import AddUserController, AddUserRequest, GetUserController
-from io_lottery.repositories import UserRepository
-from io_lottery.views import UserView
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-api = Api(app)
 
-
-class UserController(Resource):
-    def post(self):
-        dto = request.get_json()
-        return {"message": "User created", "data": dto}, 201
-
-
-
-api.add_resource(UserController, '/users')
-
-
-def handle_not_implemented_error(e):
-    return {"message": "Not Implemented"}, 501
-
+users = []
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    abort(501)
-
+    return jsonify({'error': 'Not Implemented'}), 501
 
 @app.route('/users', methods=['POST'])
-def add_user():
-    user = request.json
-    users.append(user)
-    return jsonify(user), 201
-
+def create_user():
+    new_user = request.json
+    users.append(new_user)
+    return jsonify(new_user), 201
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
-    user = next((user for user in users if user['id'] == user_id), None)
-    if not user:
-        abort(404)
-
-    user.update(request.json)
-
-    return jsonify(request.json), 200
-
+    updated_user = request.json
+    for i, user in enumerate(users):
+        if user['id'] == user_id:
+            users[i] = updated_user
+            return jsonify(updated_user), 200
+    return jsonify({'error': 'User not found'}), 404
 
 @app.route('/users/<int:user_id>', methods=['PATCH'])
 def partial_update_user(user_id):
-    user = next((user for user in users if user['id'] == user_id), None)
-    if not user:
-        abort(404)
-
-    key = list(request.json.keys())[0]
-    user[key] = request.json[key]
-
-    return jsonify(request.json), 200
-
+    user_data = request.json
+    for i, user in enumerate(users):
+        if user['id'] == user_id:
+            user.update(user_data)
+            return jsonify(user), 200
+    return jsonify({'error': 'User not found'}), 404
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    user = next((user for user in users if user['id'] == user_id), None)
-    if not user:
-        abort(404)
-    users.remove(user)
+    for i, user in enumerate(users):
+        if user['id'] == user_id:
+            del users[i]
+            return '', 204
+    return jsonify({'error': 'User not found'}), 404
 
-    return '', 204
+if __name__ == '__main__':
+    app.run()
